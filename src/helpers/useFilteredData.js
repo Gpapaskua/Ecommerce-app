d139558f6de7006619892e215b7eb999e92c5502
giorgi <g.papasqua@gmail.com>
2021-09-13 15:25:39 +0300

    added filter products with function

--- src/helpers/useFilteredData.js
+++ src/helpers/useFilteredData.js
@@ -1,17 +1,12 @@
-import { useState, useEffect } from "react"
 
 export const useFilteredData = ( filterParam, data) => {
-    const [filteredData, setFilteredData] = useState(null)
-    useEffect(() => {
         if(filterParam === 'price' && data){
             let filtered = data.sort((a, b) => a.price - b.price)
-            setFilteredData(filtered)
+            return filtered
         }
         if(filterParam === 'popular' && data){
             let filtered = data.sort((a, b) => a.rating.rate - b.rating.rate)
-            setFilteredData(filtered)
+            return filtered
         }
-    }, [filterParam])
-       
-        return filteredData
+        return data
 }
